6da3e47c80a429687e458539cd1e5d5a198f4683
Rinat273 <bairam0487@gmail.com>
2024-05-19 22:54:56 +0400

    update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,8 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+Tariff.create!(title: "Утренний", price: 1000)
+Tariff.create!(title: "Дневной", price: 800)
 
-# user1 = User.create!(email: "user1@gmail.com", encrypted_password: "123")
-# user2 = User.create!(email: "user2@gmail.com", encrypted_password: "1234")
-
-# tariff1 = Tariff.create!(title: "Утренний", price: 1000)
-# tariff2 = Tariff.create!(title: "Дневной", price: 800)
-
-# User.create!(:email => 'test@example.com', :password => 'password', :password_confirmation => 'password')
+User.create!(:email => 'test@example.com', :password => 'password', :password_confirmation => 'password')
 User.create!(:email => 'test1@example.com', :password => 'password', :password_confirmation => 'password')
